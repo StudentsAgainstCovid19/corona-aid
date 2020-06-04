150b5d30940a285864ce45e3b975a6c12428f51b
Ricardo <keksklauer4@gmail.com>
2020-06-05 01:46:53 +0200

    Fixed a bug and added console logs for debugging.

--- src/js/call_list.js
+++ src/js/call_list.js
@@ -4,6 +4,7 @@ function initCallList()
     var callListsXML = loadXMLDoc("./example_xmls/call_list.xml");
     var prioCalcXSL = loadXMLDoc("./xslt_scripts/xslt_calculate_prio.xsl");
     console.log(callListsXML);
+    console.log(prioCalcXSL)
 
     var ret = runXSLT(prioCalcXSL, callListsXML);
     console.log(ret);
