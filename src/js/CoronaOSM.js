be5a120b37607f963445e333e557b6005a40581c
Ricardo <keksklauer4@gmail.com>
2020-06-06 19:33:12 +0200

    Moved to new version of openlayers and deleted openlayers files (see issue #4).

--- src/js/CoronaOSM.js
+++ src/js/CoronaOSM.js
@@ -1,35 +1,19 @@
 
 
 function initMap() {
-		var lonlat=[8.40631,49.01175];
-        map = new OpenLayers.Map("basicMap");
-        var mapnik         = new OpenLayers.Layer.OSM();
-        var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
-        var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
-        var position       = new OpenLayers.LonLat(lonlat[0], lonlat[1]).transform( fromProjection, toProjection);
-        var zoom           = 13;
-        map.addLayer(mapnik);
-        map.setCenter(position, zoom );
-		var size = new OpenLayers.Size(100, 100);
-		// marker tests
-		var markers = new OpenLayers.Layer.Markers("Marker")
-		map.addLayer(markers);
-
-		var size = new OpenLayers.Size(25,25);
-        var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
-        OpenLayers.Icon()
-        var icon = new OpenLayers.Icon("./assets/markers/veryhigh_prio.svg", size,offset);
-
-        markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(lonlat[0],lonlat[1]).transform( fromProjection, toProjection),icon));
-
-
-
-        marker = new OpenLayers.Marker(new OpenLayers.LonLat(lonlat[0]+0.005,lonlat[1]+0.02).transform( fromProjection, toProjection),icon.clone());
-
-        marker.events.register('click', marker, function(evt) {
-            clicked_marker(2)
-        });
-        markers.addMarker(marker);
+    var lonlat=[8.40631,49.01175];
+    var map = new ol.Map({
+        target: 'map_div',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat(lonlat),
+            zoom: 13
+        })
+    });
         clicked_marker(5);
 
     // var elements = document.querySelectorAll('[id^=OpenLayers_Layer_Markers]');
