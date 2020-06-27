ac097ab5f83c79063df3cb71cd99c86e3d409acd
Ricardo <keksklauer4@gmail.com>
2020-06-27 20:42:13 +0200

    Added basic help... ;)

--- src/js/index.js
+++ src/js/index.js
@@ -127,8 +127,3 @@ function showProgressBar()
     let progressXSL = getXSLT("./xslt_scripts/xslt_progressbar.xsl");
     runXSLT(progressXSL, prioList, "progressBarDiv");
 }
-
-function showHelp()
-{
-    console.log("Hier hast du dein Hilfemenü...");
-}
