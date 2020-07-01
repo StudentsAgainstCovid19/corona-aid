beef10229d9fb3af619240581ec5aff7d5d6affc
Niklas-23 <65356992+Niklas-23@users.noreply.github.com>
2020-07-01 16:26:52 +0200

    First simple help

--- src/js/action_bar.js
+++ src/js/action_bar.js
@@ -3,7 +3,18 @@ function showHelp()
 {
     if (detail_bar === 2) return showSnackbar("Die Hilfe kann nur aufgerufen werden, wenn Sie die Patientenansicht schließen!");
     let popupWindow = document.getElementById("popup_window");
-    popupWindow.innerText="Hilfe? Wechsel doch den Schweregrad, du Noob!";
+    popupWindow.className = "helpPopup";
+    let helpPopup = document.getElementById("helpPopup");
+    helpPopup.className = "helpPopup";
 
     displayPopUp();
+}
+
+function closeHelpPopup(){
+    let filter_overlay = document.getElementById("global_overlay");
+    let popup_window = document.getElementById("popup_window");
+    filter_overlay.className = "invisible_object";
+    popup_window.className = "invisible_object";
+    let helpPopup = document.getElementById("helpPopup");
+    helpPopup.className = "invisible_object";
 }
