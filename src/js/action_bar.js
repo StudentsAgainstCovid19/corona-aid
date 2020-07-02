21ff8c47c5065f025fb3ab192c50936e67d0c7f7
Niklas-23 <65356992+Niklas-23@users.noreply.github.com>
2020-07-02 10:40:02 +0200

    Fixed bug with help-popup

--- src/js/action_bar.js
+++ src/js/action_bar.js
@@ -2,8 +2,6 @@
 function showHelp()
 {
     if (detail_bar === 2) return showSnackbar("Die Hilfe kann nur aufgerufen werden, wenn Sie die Patientenansicht schließen!");
-    let popupWindow = document.getElementById("popup_window");
-    popupWindow.className = "helpPopup";
     let helpPopup = document.getElementById("helpPopup");
     helpPopup.className = "helpPopup";
 
@@ -12,9 +10,7 @@ function showHelp()
 
 function closeHelpPopup(){
     let filter_overlay = document.getElementById("global_overlay");
-    let popup_window = document.getElementById("popup_window");
     filter_overlay.className = "invisible_object";
-    popup_window.className = "invisible_object";
     let helpPopup = document.getElementById("helpPopup");
     helpPopup.className = "invisible_object";
 }
