04bf6125176a7beb8aad5d7e51b441ad9365b0d6
Marc Auberer <marc.auberer@chillibits.com>
2020-07-05 23:43:38 +0200

    Fix double-slash

--- src/js/call_list.js
+++ src/js/call_list.js
@@ -1,15 +1,13 @@
 
-async function calculatePriorities()
-{
-    var callListsXML = loadXMLDoc(apiUrl+"/infected?compress");
-    var prioCalcXSL = getXSLT("./xslt_scripts/xslt_calculate_prio.xsl");
+async function calculatePriorities() {
+    let callListsXML = loadXMLDoc(apiUrl + "infected?compress");
+    let prioCalcXSL = getXSLT("./xslt_scripts/xslt_calculate_prio.xsl");
 
     prioList = runXSLT(prioCalcXSL, callListsXML);
     initCallList();
 }
 
-async function initCallList(openCallListBool = true)
-{
+async function initCallList(openCallListBool = true) {
     var displayCallXSL = getXSLT("./xslt_scripts/xslt_call_list.xsl");
     runXSLT(displayCallXSL, prioList, "call_list_div");
     if (openCallListBool) openCallList();
