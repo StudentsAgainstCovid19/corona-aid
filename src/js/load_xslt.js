e91c2c250372371431908d0040b14f4e822fe137
Dominic Plein <dominic-plein@gmx.de>
2020-07-14 12:45:03 +0200

    Revert "Revert "Dtds to hell""
    This reverts commit 9a3dd57d9182bdff4f8d97d7bfe1720a7ea966e8.

--- src/js/load_xslt.js
+++ src/js/load_xslt.js
@@ -1,14 +1,17 @@
-
 function getXSLT(filename) {
     let xslDoc = xsltFiles.get(filename);
     if ( !xslDoc ) {
+        // console.log("(GetXSLT) Loading XSL-File: "+filename);
         xslDoc = loadXMLDoc(filename, "text/xsl");
         xsltFiles.set(filename, xslDoc);
     }
     return xslDoc;
 }
 
-function loadXMLDoc(filename, mimeType="application/xml", errorHandlingFn = null) {
+function loadXMLDoc(filename, mimeType="application/xml", errorHandlingFn = null, callingFromGetXSLT = false) {
+    if (!callingFromGetXSLT) {
+        // console.log(`(LoadXMLDoc) loading XML-file: ${filename}`);
+    }
     try {
         let request = new XMLHttpRequest();
         request.open("GET", filename, false);
@@ -26,9 +29,9 @@ function loadXMLDoc(filename, mimeType="application/xml", errorHandlingFn = null
     }
 }
 
-
 // run an xslt script using an xml and set result as content of dom object with id
 function runXSLT(xslFile, xml, id= null) {
+    // console.log(`(RunXSLT) transforming the following xml-file: \n\n${new XMLSerializer().serializeToString(xml)}`);
     let xsltProcessor = new XSLTProcessor();
     xsltProcessor.importStylesheet(xslFile);
     if ( id ) {
--- src/xslt_scripts/xslt_pie_chart_gen.xsl
+++ src/xslt_scripts/xslt_pie_chart_gen.xsl
@@ -1,5 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+    <xsl:output
+        method="html"
+        version="1.0"
+        encoding="utf-8"/>
     <xsl:template match="chart">
         <svg xmlns="http://www.w3.org/2000/svg" width="100px" height="100px">
             <circle cx="50" cy="50" r="50" fill="yellow"/>
