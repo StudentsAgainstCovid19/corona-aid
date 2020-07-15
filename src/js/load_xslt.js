428454bf8b879913e2ee4051fce2a4e8a2255a48
Ricardo <keksklauer4@gmail.com>
2020-07-15 11:18:06 +0200

    Splitted into more js files.

--- src/js/load_xslt.js
+++ src/js/load_xslt.js
@@ -8,26 +8,6 @@ function getXSLT(filename) {
     return xslDoc;
 }
 
-function loadXMLDoc(filename, mimeType="application/xml", errorHandlingFn = null, callingFromGetXSLT = false) {
-    if (!callingFromGetXSLT) {
-        // console.log(`(LoadXMLDoc) loading XML-file: ${filename}`);
-    }
-    try {
-        let request = new XMLHttpRequest();
-        request.open("GET", filename, false);
-        if (errorHandlingFn) {
-            request.onreadystatechange = function(oEvent) {
-                errorHandlingFn(request.status);
-            };
-        }
-        request.setRequestHeader("Accept", mimeType);
-        request.send("");
-
-        return request.responseXML;
-    } catch (e) {
-        serviceUnavailableError();
-    }
-}
 
 // run an xslt script using an xml and set result as content of dom object with id
 function runXSLT(xslFile, xml, id= null) {
@@ -41,22 +21,4 @@ function runXSLT(xslFile, xml, id= null) {
     } else {
         return xsltProcessor.transformToDocument(xml); // Return only in the else branch ???
     }
-}
-
-function postRequest(res, content) {
-    let parser = new DOMParser();
-    let request = new XMLHttpRequest();
-    request.open("POST", apiUrl + res, true);
-    request.setRequestHeader("Content-type", "application/xml");
-    request.setRequestHeader("Accept", "application/xml");
-
-    request.send(parser.parseFromString(content,"application/xml"));
-}
-
-function putRequest(res, content= null) {
-    let request = new XMLHttpRequest();
-    request.open("PUT", apiUrl + res, true);
-    request.setRequestHeader("Content-type", "application/xml");
-    request.setRequestHeader("Accept", "application/xml");
-    request.send(content);
 }
--- src/js/tooltip_position.js
+++ src/js/tooltip_position.js
@@ -1,15 +1,11 @@
-    var cumulativeOffset = function(element){
-        var scrolltop = 0, screenHeight, top = 0, welement = element;
-        do{
-            top = element.offsetTop;
-            scrolltop = element.parentElement.parentElement.scrollTop || 0;
-            screenHeight = top - scrolltop + Math.floor(element.offsetHeight / 2)-10;
-            welement = welement.offsetParent;
-        } while(welement);
+var cumulativeOffset = function(element){
+    let scrolltop = 0, screenHeight, top = 0, welement = element;
+    do{
+        top = element.offsetTop;
+        scrolltop = element.parentElement.parentElement.scrollTop || 0;
+        screenHeight = top - scrolltop + Math.floor(element.offsetHeight / 2)-10;
+        welement = welement.offsetParent;
+    } while(welement);
 
-        console.log("top :" + top + " | element: " + element + " | berechnet: " + screenHeight + " | scrollTop: " + scrolltop);
-        console.log(element);
-        var temp = element.childNodes[0].getElementsByClassName("tooltiptext")[0].style.top = screenHeight + "px";
-        console.log(temp);
-    };
+};
 
